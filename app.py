fcf501bbb32aadbd3e4663bd2a5f6ce1ace0a17d
Prince Prajapati <prince07190@gmail.com>
2025-04-05 22:35:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -138,8 +138,9 @@ def stream():
         
     return Response(generate(), mimetype='text/event-stream')
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     os.makedirs('templates', exist_ok=True)
     os.makedirs('static', exist_ok=True)
-    
-    app.run(debug=True, port=5000)
+
+    port = int(os.environ.get('PORT', 5000))  
+    app.run(host='0.0.0.0', port=port, debug=True)
